487d282f3483a6cbc2a9dcc7d5b7ccf33a4bfbca
ArthurVictor42 <arthurv.santos@unifacol.edu.br>
2026-03-15 10:11:52 -0300

    upload do arquivo inicial

---
+++ src/main/java/com/example/alocacaoimovel/AlocacaoImovelApplication.java
@@ -0,0 +1,13 @@
+package com.example.alocacaoimovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlocacaoImovelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlocacaoImovelApplication.class, args);
+    }
+
+}
